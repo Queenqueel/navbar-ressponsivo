7c07d4e07162486559ce930ddca0731c1dcf893e
Queenqueel <raquel.f.p.500@gmail.com>
2022-09-16 23:59:27 -0300

    addded modal login/register

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function menuShow() {
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 }
+
+function signupToggle() {
+    var container = document.querySelector('.container');
+    container.classList.toggle('active');
+    var popup = document.querySelector('.signup-form');
+    popup.classList.toggle('active');
+}
+
+function loginToggle() {
+    var container = document.querySelector('.container');
+    container.classList.toggle('active');
+    var popup = document.querySelector('.login-form');
+    popup.classList.toggle('active');
+}
